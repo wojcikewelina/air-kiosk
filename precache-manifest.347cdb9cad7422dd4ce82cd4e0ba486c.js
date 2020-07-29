313af3596a993598c1018c349f9307c0d1dc9dd7
wojcikewelina <gargula.ewe@gmail.com>
2020-07-29 22:32:12 +0200

    Updates

---
+++ precache-manifest.347cdb9cad7422dd4ce82cd4e0ba486c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f69a6875bc13016287cd9bf18860760",
+    "url": "/air-kiosk/index.html"
+  },
+  {
+    "revision": "04208e1897d9c01222d3",
+    "url": "/air-kiosk/static/css/2.7cd5cc06.chunk.css"
+  },
+  {
+    "revision": "d5035539e736cc914aec",
+    "url": "/air-kiosk/static/css/main.912899d7.chunk.css"
+  },
+  {
+    "revision": "04208e1897d9c01222d3",
+    "url": "/air-kiosk/static/js/2.63acaa80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/air-kiosk/static/js/2.63acaa80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5035539e736cc914aec",
+    "url": "/air-kiosk/static/js/main.2590a404.chunk.js"
+  },
+  {
+    "revision": "7dc017cd7bb72f7e66b5",
+    "url": "/air-kiosk/static/js/runtime-main.8e953ca7.js"
+  },
+  {
+    "revision": "ecda52de8dd348af0c81bf4e5ac686c8",
+    "url": "/air-kiosk/static/media/Carousel-1.ecda52de.jpg"
+  },
+  {
+    "revision": "3a1363db1b47644d5b00ec5cf5241eeb",
+    "url": "/air-kiosk/static/media/Carousel-2.3a1363db.jpg"
+  },
+  {
+    "revision": "f7191f3f587130ca647c968e3ba3a2e2",
+    "url": "/air-kiosk/static/media/Carousel-3.f7191f3f.jpg"
+  },
+  {
+    "revision": "9090a861053816b9c493a05658b4f7aa",
+    "url": "/air-kiosk/static/media/pop2.9090a861.jpg"
+  },
+  {
+    "revision": "6876a1d3118429ffc7d8a1329b801910",
+    "url": "/air-kiosk/static/media/pop4.6876a1d3.jpg"
+  },
+  {
+    "revision": "bdf0b49ceeceb5ec1685056e8e030533",
+    "url": "/air-kiosk/static/media/skarbonka.bdf0b49c.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/air-kiosk/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/air-kiosk/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/air-kiosk/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/air-kiosk/static/media/slick.f97e3bbf.svg"
+  }
+]);
